738a7632f6519e11f59d3221f8fe88397688f302
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2022-11-10 21:49:12 +0530

    JavaScript Fundamental Part -1 > conditional or ternary operators coding challenge #4 completed

--- JAVSCRIPT-FUNDAMENTAL-PART-1/assignment.js
+++ JAVSCRIPT-FUNDAMENTAL-PART-1/assignment.js
@@ -377,6 +377,7 @@ console.log("No one win")
 
 //////////////// CONDITIONAL OR TERNARY OPERATOR ASSIGNMENTS
 
+/* 
 const country = "India";
 const population = 23;
 
@@ -385,6 +386,46 @@ console.log(population >= 33 ? `${country} population is above average` : `${cou
 
 // second way to use ternary operator with expression and template literals to get the result on console log
 console.log(`${country}'s population is ${population >= 33 ? "above" : "below"} average`);
+
+
+*/
+
+
+
+//////////////// CONDITIONAL OR TERNARY OPERATOR CODING CHALLENGE
+
+/* Coding Challenge #4
+
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 �
+
+ */
+
+
+const bill = 430;
+
+// number 1
+const tip = bill <= 300 && bill >= 50  ? bill * 0.15 : bill * 0.2;  // conditional operator  output in console is if the bill is less then or equal to 300 and greater than and equal to 50 so then if block is executed so therfore the first part of ternary operator will be executed and become the result of this operator essentially. So the result of this operator will be bill times 0.15 and that's the value that will be assigned  to the tip variable.
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill + tip}`); // template literal
+
+// true and false gives false
+
+
 
 
 
