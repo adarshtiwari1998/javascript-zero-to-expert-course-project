6a734a51e2f74bec4750e76c516748b5ae47509a
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2022-12-02 03:43:50 +0530

    JavaScript Fundamental Part-2 > coding challenge #4 commenting code done

--- JAVASCRIPT-FUNDAMENTAL-PART-2/assignment.js
+++ JAVASCRIPT-FUNDAMENTAL-PART-2/assignment.js
@@ -690,13 +690,13 @@ COMMENT END */
 /* ############ CODING CHALLENGE #4 ############  */
 
 
+
 /*
 Coding Challenge #4
 Let's improve Steven's tip calculator even more, this time using loops!
 Your tasks:
 1. Create an array 'bills' containing all 10 test bill values
 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
-
 
 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
 tips and total values (bill + tip) for every bill value in the bills array. Use a for
@@ -705,7 +705,6 @@ Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
 tips and totals arrays �
 Bonus:
-
 
 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
 an argument. This function calculates the average of all numbers in the given
@@ -723,6 +722,10 @@ GOOD LUCK �
 
 
 */
+
+/* COMMENT START
+
+
 
 // No 1 and 2
 
@@ -754,31 +757,7 @@ console.log(bills);
 console.log(tips);
 console.log(totals);
 
-
-
-/*
-4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
-an argument. 
-
-This function calculates the average of all numbers in the given
-array. 
-
-This is a difficult challenge (we haven't done this before)! Here is how to
-solve it:
-
-4.1. First, you will need to add up all values in the array. To do the addition,
-start by creating a variable 'sum' that starts at 0. Then loop over the
-array using a for loop. In each iteration, add the current value to the
-'sum' variable. This way, by the end of the loop, you have all values
-added together
-4.2. To calculate the average, divide the sum you calculated before by the
-length of the array (because that's the number of elements)
-4.3. Call the function with the 'totals' array
-GOOD LUCK �
-
-*/
-
-// const arr = [];
+// BONUS LOGIC
 
 const calcAverage = function(arr) { // we will receive an array here, any array will.
 // So once again, we are creating generic function, and it's does not care what kind of value he receive. It can receive, bllls, tips, masses, heights, etc. It doesn't care what the values are.
@@ -804,6 +783,10 @@ console.log("=============Tips");
 console.log(calcAverage(tips));
 console.log("=============Total");
 console.log(calcAverage(totals));
+
+
+
+COMMENT END */
 
 
 
